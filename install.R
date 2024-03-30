50ac66d785749f85eb361670e428e5b129f9179e
Shawn <sgraven66@gmail.com>
2024-03-29 21:07:40 -0500

    This very well could be an issue for lintr

--- install.R
+++ install.R
@@ -1,3 +1,3 @@
-install.packages(c("tidyverse","DBI","RMariaDB","shinyjs"))
-install.packages(c("sass", "bsicons"))
-devtools::install_github("rstudio/pool")
+# install.packages(c("tidyverse","DBI","RMariaDB","shinyjs"))
+# install.packages(c("sass", "bsicons"))
+# devtools::install_github("rstudio/pool")
